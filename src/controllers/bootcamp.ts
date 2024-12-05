d6eb47e2886aabe1b7ef3db988eed833a1df5fd1
heismyke <81974207+heismyke@users.noreply.github.com>
2024-12-05 15:25:52 +0100

    added new handle functions

--- src/controllers/bootcamp.ts
+++ src/controllers/bootcamp.ts
@@ -3,10 +3,54 @@ import { Request, Response } from 'express';
 //@desc        GET all bootcamps
 //@route       GET /api/v1/bootcamps
 //@access      Public
-
 export const getBootcamps = (req: Request, res: Response) => {
    res.status(200).json({
        success : true,
        msg : "Welcome to devcamper api"
    })
+}
+
+
+//@desc         GET a single bootcamp
+//@route        GET /api/v1/bootcamps/:id
+//@access       Private
+export const getBootcamp = (req: Request, res: Response) => {
+    res.status(200).json({
+        success : true,
+        msg : "Welcome to devcamper api"
+    })
+}
+
+
+//@desc         CREATE a bootcamp
+//@route        POST /api/v1/bootcamps
+//@access       Private
+export const createBootcamp = (req: Request, res: Response) => {
+    res.status(200).json({
+        success : true,
+        msg : "Welcome to devcamper api"
+    })
+}
+
+
+
+//@desc         UPDATE a bootcamp
+//@route        PUT /api/v1/bootcamps/:id
+//@access       Private
+export const updateBootcamp = (req: Request, res: Response) => {
+    res.status(200).json({
+        success : true,
+        msg : "Welcome to devcamper api"
+    })
+}
+
+
+//@desc         DELETE a bootcamp
+//@route        DELETE /api/v1/bootcamps/:id
+//@access       Private
+export const deleteBootcamp = (req: Request, res: Response) => {
+    res.status(200).json({
+        success : true,
+        msg : "Welcome to devcamper api"
+    })
 }
